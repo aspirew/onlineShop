05cad6acaf61d4fd5230475b0a2babfb499f6585
rafal <rafal1behrendt@gmail.com>
2020-12-09 21:29:09 +0100

    product image edit done

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { productData } from '../interfaces';
+import { ImageService } from './image.service';
 
 interface status {
   success: boolean,
   message: string
 }
 
-interface imageUploadStatus {
-  success: boolean,
-  name: string,
-  message: string
-}
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private image: ImageService) { }
 
   editProduct(id, editData){
     return this.http.post<status>(`/api/product/${id}/edit`, editData)
   }
 
   async uploadProduct(productData, data: FormData){
-    const imageUploadRes = await this.http.post<imageUploadStatus>('/api/product/addImage', data).toPromise()
+    const imageUploadRes = await this.image.uploadImage(data).toPromise();
     if(imageUploadRes.success){
       const name = imageUploadRes.name
       return await this.http.post<status>('/api/product/add', {productData, name}).toPromise()
